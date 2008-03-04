a9945b2f9dfd2b61c1f9f1b586802c310000f25b
parth.technofreak <parth.technofreak@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-03-04 19:09:04 +0000

    forgot to add the mailer scripts, ooops! did now

---
+++ software/SupportScripts/trunk/sbin/rbc_host_keys_mailer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Mailing public keys..."
+filemailer -s "SSH Host: $(cat /etc/hostid)" -n -a /etc/ssh/ssh_host_dsa_key.pub -a /etc/ssh/ssh_host_rsa_key.pub hostkey@"$MAILDOMAIN"
+filemailer -s "SSH tcconfig: $(cat /etc/hostid)" -n -a ~tcconfig/.ssh/id_rsa.pub -a ~tcconfig/.ssh/id_dsa.pub hostkey@"$MAILDOMAIN"
