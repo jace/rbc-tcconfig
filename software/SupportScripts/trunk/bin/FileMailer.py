4a2eb135a704a82eb037128db207341c6b170b69
jackerhack <jackerhack@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-03-04 06:17:10 +0000

    Updated FileMailer to check that attachments do not have duplicate names. Added a new zipattach option to compress all attachments.

--- software/SupportScripts/trunk/bin/FileMailer.py
+++ software/SupportScripts/trunk/bin/FileMailer.py
@@ -3,7 +3,7 @@
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
-from email.Utils import COMMASPACE, formatdate
+from email.Utils import formatdate
 from email import Encoders
 import os
 import pwd
@@ -20,35 +20,40 @@ except ImportError:
     from StringIO import StringIO
 
 class FileMailer:
-    """Main class for mails"""
+    """FileMailer sends binary files over email as Base64-encoded attachments."""
 
-    def __init__(self,recipients , subject="", text=""):
+    def __init__(self, recipients, subject="", text=""):
         self.smtpserver = None
         self.recipients = recipients
         self.subject = subject
         self.text = text
+        self.msgfrom = self.__getFrom()
         self.msg = MIMEMultipart()
-        self.msg['From'] = self.__getFrom()
+        self.msg['From'] = self.msgfrom
         self.msg['To'] = ", ".join(recipients)
         self.msg['Date'] = formatdate(localtime=True)
         self.msg['Subject'] = subject
-        self.msg.attach( MIMEText(text) )
+        self.msg.attach(MIMEText(text))
+        self.filenames = {}
 
     def __getFrom(self):
         return pwd.getpwuid(os.geteuid())[0] + '@' + socket.getfqdn()
 
-    def send(self, server, port):
+    def send(self, server, port=25):
+        """Send the message via the specified SMTP server."""
         self.smtpserver = SMTP(server, port)
-        self.smtpserver.sendmail(self.__getFrom(), self.recipients, self.msg.as_string() )
+        self.smtpserver.sendmail(self.msgfrom, self.recipients, self.msg.as_string())
         self.smtpserver.close()
 
     def attach(self, filename, filedata):
-        """Function to send the mails
-        """
-        #for filetobesend in filestobesend:
-        part = MIMEBase('application', "octet-stream")
+        """Attach a file to the message."""
+        filename = os.path.basename(filename)
+        if filename in self.filenames:
+            raise ValueError, "Attachment with that name already present."
+        self.filenames[filename] = None # Add to list
+        part = MIMEBase('application', "octet-stream") # Send everything as a binary attachment
         if not hasattr(filedata, 'read'):
-            filedata = file(filedata, 'rb').read()
+            filedata = open(filedata, 'rb').read()
         else:
             filedata = filedata.read()
         part.set_payload(filedata)
@@ -56,9 +61,13 @@ class FileMailer:
         part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(filename))
         self.msg.attach(part)
 
-    def attachtext(self, filename, text):
+    def attachzipped(self, filename, filedata):
+        if not hasattr(filedata, 'read'):
+            filedata = open(filedata, 'rb').read()
+        else:
+            filedata = filedata.read()
         zipped = StringIO()
-        zipped.write(bz2.compress(text))
+        zipped.write(bz2.compress(filedata))
         zipped.seek(0)
         self.attach(filename + '.bz2', zipped)
 
@@ -70,18 +79,20 @@ def main(argv):
     parser.add_option('-n', '--nobody', action="store_true", default=False,
                       help="Do not read body text from stdin [default is to read]")
     parser.add_option('-z', '--zip', action="store_true", default=False,
-                      help="Compress text from standard input [default False]")
+                      help="Compress text from standard input [default %default]")
     parser.add_option('-a', '--attachment', type="string", action="append", dest="files",
                       help="Attach file")
+    parser.add_option('-Z', '--zipattach', action="store_true", default=False,
+                      help="Compress all attachments [default %default]")
     parser.add_option('-r', '--relayhost', type="string", default="localhost",
-                      help="Mail relay host [default localhost]")
+                      help="Mail relay host [default %default]")
     parser.add_option('-p', '--port', type="int", default=25,
-                      help="Mail server port [default 25]")
+                      help="Mail server port [default %default]")
     (options, recipients) = parser.parse_args(argv[1:])
 
     if len(recipients) < 1:
-        print >> sys.stderr, "At least one recipient must be specified."
-        return -1
+        print >> sys.stderr, "At least one recipient must be specified. Use -h for details."
+        return 1
 
     # Do something here
     default_text = "FileMailer with arguments: %s\r\n" % " ".join([repr(arg) for arg in argv])
@@ -96,10 +107,13 @@ def main(argv):
 
     filemailer = FileMailer(recipients, options.subject, body_text)
     if options.zip and user_text != '':
-        filemailer.attachtext("stdin.txt", user_text)
+        filemailer.attachzipped("stdin.txt", user_text)
     if options.files != None:
         for attachment in options.files:
-            filemailer.attach(attachment, attachment)
+            if options.zipattach:
+                filemailer.attachzipped(attachment, attachment)
+            else:
+                filemailer.attach(attachment, attachment)
     filemailer.send(options.relayhost, options.port)
 
 if __name__ == "__main__":
