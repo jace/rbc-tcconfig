80878440a912391227ffb70c5f2c4d4e81c29a04
jackerhack <jackerhack@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-04-30 06:38:36 +0000

    More work -- seems to work now. extended testing pending.

--- software/ServerScripts/trunk/rbc_mailadd.py
+++ software/ServerScripts/trunk/rbc_mailadd.py
@@ -3,47 +3,74 @@
 # Script to create a new mail account in a Postfix-MySQL environment. Assumes
 # database structure of PostfixAdmin.
 
-import os
 import sys
 from optparse import OptionParser
 from ConfigParser import ConfigParser
 import MySQLdb
 
+# Taken from the PHP sources for PostfixAdmin:
 # INSERT INTO alias (address,goto,domain,created,modified,active) VALUES ('$fUsername','$fUsername','$fDomain',NOW(),NOW(),'$fActive')
 # INSERT INTO mailbox (username,password,name,maildir,quota,domain,created,modified,active) VALUES ('$fUsername','$password','$fName','$maildir','$quota','$fDomain',NOW(),NOW(),'$fActive')
 
 def sqlquote(sqlstring):
     return sqlstring.replace("'", "''")
 
-def run(username, domain, dbconfig):
+def run(username, domain, password, dbconfig):
     # Attempt to connect to database and make an account
     dbhost = dbconfig.get('Details', 'host')
     dbname = dbconfig.get('Details', 'database')
     dbuser = dbconfig.get('Details', 'username')
     dbpass = dbconfig.get('Details', 'password')
-    maildirpath = dbconfig.get('Details', 'maildirpath')
+
+    email = '%s@%s' % (username, domain)
 
     con = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpass, db=dbname)
     cur = con.cursor()
 
-    cur.execute("INSERT INTO alias (address,goto,domain,created,modified,active) VALUES ('%s', '%s', '%s', NOW(), NOW(), TRUE)" % (sqlquote(username), sqlquote(username), sqlquote(domain)))
-    cur.execute("INSERT INTO mailbox (username,password,name,maildir,quota,domain,created,modified,active) VALUES ('%s', '', '%s', '%s', 0, '%s', NOW(), NOW(), TRUE)" % (sqlquote(username), sqlquote(username), sqlquote(os.path.join(maildirpath, domain, username), sqlquote(domain))))
+    cur.execute("SELECT * from alias WHERE address='%s' AND domain='%s'" %
+            (sqlquote(email), sqlquote(domain)))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute("INSERT INTO alias (address,goto,domain,created,modified,active) "\
+                "VALUES ('%s', '%s', '%s', NOW(), NOW(), TRUE)" %
+                (sqlquote(email), sqlquote(email), sqlquote(domain)))
+    cur.execute("SELECT * from mailbox WHERE username='%s' AND domain='%s'" %
+            (sqlquote(email), sqlquote(domain)))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute("INSERT INTO mailbox "\
+                "(username,password,name,maildir,quota,domain,created,modified,active) "\
+                "VALUES ('%s', '%s', '%s', '%s', 0, '%s', NOW(), NOW(), TRUE)" %
+                (sqlquote(email), sqlquote(password), sqlquote(username),
+                    sqlquote(email+'/'), sqlquote(domain)))
+    elif password: # Non-blank password? Apply it
+        cur.execute("UPDATE mailbox SET password='%s' WHERE username='%s' AND domain='%s'" %
+                (sqlquote(password), sqlquote(username), sqlquote(domain)))
+    return 0
 
 def main(argv):
+    """Usage: %prog options
+    If the mailbox already exists, only the password, if specified, will be updated
+    """
     parser = OptionParser(usage=main.__doc__)
 
     parser.add_option('-c', '--configfile', type='string',
             default='/etc/tcconfig/mailsetup.cfg',
-            help="Path to mail configuration [%default]")
+            help="path to mail configuration [%default]")
     parser.add_option('-u', '--username', type='string', default=None,
-            help="Username to create account for")
-    parser.add_option('-D', '--domain', type='string', default=None,
-            help="Domain under which to create user account")
+            help="username to create account for")
+    parser.add_option('-d', '--domain', type='string', default=None,
+            help="domain under which to create user account")
+    parser.add_option('-p', '--password', type='string', default=None,
+            help="set encrypted string as mailbox password")
 
     (options, args) = parser.parse_args(argv[1:])
 
     username = options.username
     domain = options.domain
+    password = options.password
+    if not password:
+        password = ''
 
     if not username:
         print >> sys.stderr, "Username not specified. Use -h for help."
@@ -67,7 +94,7 @@ def main(argv):
         print >> sys.stderr, "Could not read config file: %s" % options.configfile
         return 4
 
-    run(username, domain, dbconfig)
+    return run(username, domain, password, dbconfig)
 
 
 if __name__=='__main__':
