b1ae8b5568f0c21f5f512f8ba571ab8cb967ffe6
kartik.mistry <kartik.mistry@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-05-08 11:28:50 +0000

    Added wpasupplicant network-manager network-manager-gnome in script

--- software/DeveloperTools/trunk/apt-remove-unwanted.sh
+++ software/DeveloperTools/trunk/apt-remove-unwanted.sh
@@ -1,25 +1,26 @@
 #! /bin/sh
 
 apt-get remove --ignore-missing --auto-remove \
-gnome-games f-spot gimp gimp-data gimp-python xsane libgimp2.0 gnome-utils \
-gthumb ekiga evolution gaim gaim-data gnome-app-install gnome-media gnome-orca \
-gtkhtml3.14 libeel2-2 libgail-common libgail-gnome-module libgail18 libgnome2.0-cil \
+gnome-games f-spot gimp gimp-data gimp-python xsane libgimp2.0 gnome-utils gthumb \
+ekiga evolution gaim gaim-data gnome-app-install gnome-media gnome-orca gtkhtml3.14 \
+libeel2-2 libgail-common libgail-gnome-module libgail18 libgnome2.0-cil wireless-tools \
 libgtkhtml2-0 libgtkhtml3.14-19 nautilus nautilus-cd-burner python-gtkhtml2 tomboy \
 aspell aspell-en dictionaries-common gnome-spell language-support-en myspell-en-gb \
 myspell-en-us myspell-en-za openoffice.org openoffice.org-base openoffice.org-calc \
 openoffice.org-common openoffice.org-core openoffice.org-draw openoffice.org-filter-mobiledev \
 openoffice.org-gnome openoffice.org-gtk openoffice.org-help-en-us openoffice.org-hyphenation \
-openoffice.org-impress openoffice.org-java-common openoffice.org-l10n-common \
-openoffice.org-l10n-en-gb openoffice.org-l10n-en-za openoffice.org-math openoffice.org-style-human \
+openoffice.org-impress openoffice.org-java-common openoffice.org-l10n-common capplets-data \
+openoffice.org-l10n-en-gb openoffice.org-l10n-en-za openoffice.org-math network-manager-gnome \
 openoffice.org-thesaurus-en-us openoffice.org-writer python-uno thunderbird-locale-en-gb \
-wamerican wbritish deskbar-applet gedit libtotem-plparser1 python-gnome2-desktop python-gnome2-extras \
-rhythmbox serpentine totem totem-gstreamer totem-mozilla sound-juicer gcalctool wireless-tools \
+wamerican wbritish deskbar-applet gedit libtotem-plparser1 python-gnome2-desktop \
+rhythmbox serpentine totem totem-gstreamer totem-mozilla sound-juicer gcalctool \
 bittorrent evince example-content laptop-detect laptop-mode-tools tangerine-icon-theme \
-tango-icon-theme telnet dosfstools espeak app-install-data app-install-data-commercial capplets-data \
+tango-icon-theme telnet dosfstools espeak app-install-data app-install-data-commercial \
 cdrecord evolution-data-server-common genisoimage libebook1.2-9 libecal1.2-7 libcamel1.2-10 \
 libedata-book1.2-2 libedata-cal1.2-6 libedataserver1.2-9 libedataserverui1.2-8 libegroupwise1.2-13 \
 libexchange-storage1.2-3 xscreensaver-data xscreensaver-gl ttf-opensymbol libbluetooth2 \
-binfmt-support ubuntu-desktop aptitude bc compiz gdebi gedit-common gnome-about \
-gnome-applets-data gnome-cards-data gnome-games-data gnome-mag gnome-panel-data \
-gucharmap scim scim-gtk2-immodule scim-modules-socket scim-tables-additional synaptic wodim \
-update-manager update-manager-core ubuntu-artwork ubuntu-docs ubuntu-sounds
+binfmt-support ubuntu-desktop aptitude bc compiz gdebi gedit-common gnome-about wodim \
+gnome-applets-data gnome-cards-data gnome-games-data gnome-mag gnome-panel-data gucharmap \
+scim scim-gtk2-immodule scim-modules-socket scim-tables-additional synaptic update-manager \
+update-manager-core ubuntu-artwork ubuntu-docs ubuntu-sounds wpasupplicant network-manager \
+python-gnome2-extras openoffice.org-style-human
