77a69ec9a4654c61ef713631d23dbac491168999
kartik.mistry <kartik.mistry@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-05-13 10:43:46 +0000

    Indented list to 80 chars and added few more packages to remove, 100M free..

--- software/DeveloperTools/trunk/apt-remove-unwanted.sh
+++ software/DeveloperTools/trunk/apt-remove-unwanted.sh
@@ -1,31 +1,41 @@
 #! /bin/sh
 
 apt-get remove --ignore-missing --auto-remove --purge \
-gnome-games f-spot gimp gimp-data gimp-python xsane libgimp2.0 gnome-utils gthumb \
-ekiga evolution gaim gaim-data gnome-app-install gnome-media gnome-orca gtkhtml3.14 \
-libeel2-2 libgail-common libgail-gnome-module libgail18 libgnome2.0-cil wireless-tools \
-libgtkhtml2-0 libgtkhtml3.14-19 nautilus nautilus-cd-burner python-gtkhtml2 tomboy \
-aspell aspell-en dictionaries-common gnome-spell language-support-en myspell-en-gb \
-myspell-en-us myspell-en-za openoffice.org openoffice.org-base openoffice.org-calc \
-openoffice.org-common openoffice.org-core openoffice.org-draw openoffice.org-filter-mobiledev \
-openoffice.org-gnome openoffice.org-gtk openoffice.org-help-en-us openoffice.org-hyphenation \
-openoffice.org-impress openoffice.org-java-common openoffice.org-l10n-common capplets-data \
-openoffice.org-l10n-en-gb openoffice.org-l10n-en-za openoffice.org-math network-manager-gnome \
-openoffice.org-thesaurus-en-us openoffice.org-writer python-uno thunderbird-locale-en-gb \
-wamerican wbritish deskbar-applet gedit libtotem-plparser1 python-gnome2-desktop \
-rhythmbox serpentine totem totem-gstreamer totem-mozilla sound-juicer gcalctool \
-bittorrent evince example-content laptop-detect laptop-mode-tools tangerine-icon-theme \
-tango-icon-theme telnet dosfstools espeak app-install-data app-install-data-commercial \
-cdrecord evolution-data-server-common genisoimage libebook1.2-9 libecal1.2-7 libcamel1.2-10 \
-libedata-book1.2-2 libedata-cal1.2-6 libedataserver1.2-9 libedataserverui1.2-8 libegroupwise1.2-13 \
-libexchange-storage1.2-3 xscreensaver-data xscreensaver-gl ttf-opensymbol libbluetooth2 \
-binfmt-support ubuntu-desktop aptitude bc compiz gdebi gedit-common gnome-about wodim \
-gnome-applets-data gnome-cards-data gnome-games-data gnome-mag gnome-panel-data gucharmap \
-scim scim-gtk2-immodule scim-modules-socket scim-tables-additional synaptic update-manager \
-update-manager-core ubuntu-artwork ubuntu-docs ubuntu-sounds wpasupplicant network-manager \
-python-gnome2-extras openoffice.org-style-human popularity-contest nautilus-data \
-brltty cdrdao command-not-found fortune-mod fortunes-min software-properties-gtk \
-ttf-arabeyes ttf-arphic-ukai ttf-arphic-uming ttf-baekmuk ttf-bengali-fonts vino \
-ttf-devanagari-fonts ttf-gujarati-fonts ttf-malayalam-fonts ttf-oriya-fonts yelp \
-ttf-punjabi-fonts ttf-tamil-fonts ttf-telugu-fonts ttf-kochi-gothic ttf-kochi-mincho \
-ttf-lao ttf-thai-tlwg vnc-common w3m at-spi tpshset
+gnome-games f-spot gimp gimp-data gimp-python xsane libgimp2.0 gnome-utils \
+gthumb ekiga evolution gaim gaim-data gnome-app-install gnome-media \
+gnome-orca gtkhtml3.14 libeel2-2 libgail-common libgail-gnome-module \
+libgail18 libgnome2.0-cil wireless-tools libgtkhtml2-0 libgtkhtml3.14-19 \
+nautilus nautilus-cd-burner python-gtkhtml2 tomboy aspell aspell-en \
+dictionaries-common gnome-spell language-support-en myspell-en-gb \
+myspell-en-us myspell-en-za openoffice.org openoffice.org-base \
+openoffice.org-calc openoffice.org-common openoffice.org-core \
+openoffice.org-draw openoffice.org-filter-mobiledev openoffice.org-gnome \
+openoffice.org-gtk openoffice.org-help-en-us openoffice.org-hyphenation \
+openoffice.org-impress openoffice.org-java-common openoffice.org-l10n-common \
+capplets-data openoffice.org-l10n-en-gb openoffice.org-l10n-en-za \
+openoffice.org-math network-manager-gnome openoffice.org-thesaurus-en-us \
+openoffice.org-writer python-uno thunderbird-locale-en-gb wamerican wbritish \
+deskbar-applet gedit libtotem-plparser1 python-gnome2-desktop rhythmbox \
+serpentine totem totem-gstreamer totem-mozilla sound-juicer gcalctool \
+bittorrent evince example-content laptop-detect laptop-mode-tools \
+tangerine-icon-theme tango-icon-theme telnet dosfstools espeak \
+app-install-data app-install-data-commercial cdrecord vino yelp \
+evolution-data-server-common genisoimage libebook1.2-9 libecal1.2-7 \
+libcamel1.2-10 libedata-book1.2-2 libedata-cal1.2-6 libedataserver1.2-9 \
+libedataserverui1.2-8 libegroupwise1.2-13 libexchange-storage1.2-3 \
+xscreensaver-data xscreensaver-gl ttf-opensymbol libbluetooth2 binfmt-support \
+ubuntu-desktop aptitude bc compiz gdebi gedit-common gnome-about wodim \
+gnome-applets-data gnome-cards-data gnome-games-data gnome-mag scim \
+gnome-panel-data gucharmap scim-gtk2-immodule scim-modules-socket \
+scim-tables-additional synaptic update-manager update-manager-core \
+ubuntu-artwork ubuntu-docs ubuntu-sounds wpasupplicant network-manager \
+python-gnome2-extras openoffice.org-style-human popularity-contest \
+nautilus-data brltty cdrdao command-not-found fortune-mod fortunes-min \
+software-properties-gtk ttf-arabeyes ttf-arphic-ukai ttf-arphic-uming \
+ttf-baekmuk ttf-bengali-fonts ttf-devanagari-fonts ttf-gujarati-fonts \
+ttf-malayalam-fonts ttf-oriya-fonts ttf-punjabi-fonts ttf-tamil-fonts \
+ttf-telugu-fonts ttf-kochi-gothic ttf-kochi-mincho ttf-lao ttf-thai-tlwg \
+vnc-common w3m at-spi toshset compiz-gtk compiz-plugins diveintopython \
+eog gconf2 gconf2-common gconf-editor gdebi-core rss-glx gnome-terminal-data \
+gnome-media-common gnome-netstatus-applet gnome-nettool gnome-screensaver \
+gnome-system-monitor gnome-system-tools
