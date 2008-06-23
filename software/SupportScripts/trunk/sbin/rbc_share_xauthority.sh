104002284d101739485668dd3b7022230ffbbd52
kushaldas <kushaldas@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-06-23 08:14:50 +0000

    Chnaged to have *user* account instead *opeartor* account

--- software/SupportScripts/trunk/sbin/rbc_share_xauthority.sh
+++ software/SupportScripts/trunk/sbin/rbc_share_xauthority.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Script to share the 'operator' user's ~/.Xauthority file with another user
+# Script to share the 'user' user's ~/.Xauthority file with another user
 # Can be dangerous in a true multi-user system -- do not reuse elsewhere.
 
 rm -f /home/"$1"/.Xauthority
-cp /home/operator/.Xauthority /home/"$1"/.Xauthority
+cp /home/user/.Xauthority /home/"$1"/.Xauthority
 chown "$1":"$1" /home/"$1"/.Xauthority
