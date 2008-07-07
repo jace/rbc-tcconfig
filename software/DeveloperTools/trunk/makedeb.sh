d18e5a347deaf8c378b33176bd3709d92692e124
pj.pandit <pj.pandit@cf2606c8-b33c-0410-b105-fbdef12609a6>
2008-07-07 11:14:16 +0000

    Did change the makedeb.sh to build an entire sub-tree under the given directory. Also fixed some minor typo.

--- software/DeveloperTools/trunk/makedeb.sh
+++ software/DeveloperTools/trunk/makedeb.sh
@@ -1,54 +1,71 @@
 #!/bin/bash
-
+#
 # Script to re-create deb files if the corresponding EPM list files have
 # been updated. Works in two ways:
 #
-#  1. If list files have been specified, processes them
-#  2. If a folder has been specified, looks for list files within
+# 1. If list files have been specified, processes them
+# 2. If a folder has been specified, looks for list files within
+#
+# Make sure all paths are *absolute* paths, beginning with slash (`/').
+#
 
-if [ "$#" -eq 0 ]; then
-    echo "Usage: makedeb.sh /path/to/folder ..."
-    echo "       makedeb.sh file1.list file2.list ..."
-    echo
+prog=`basename $0`;
+
+usage ()
+{
+    echo "Usage: $prog /path/to/folder ..."
+    echo -e "       $prog file-1.list file-2.list ...\n"
     echo "List files and folders can be specified in the same invocation."
-    exit 1
-fi
+}
 
-makelist () {
-    DIRNAME="`dirname \"$1\"`"
-    LISTFILE="`basename \"$1\"`"
-    PRODUCT="${LISTFILE%%.list}"
-    VERSION=$(grep %version "$LISTFILE" | head -1 | sed -e "s/$(echo -e '\t')/ /g" | cut -f2 -d\ )
-    RELEASE=$(grep %release "$LISTFILE" | head -1 | sed -e "s/$(echo -e '\t')/ /g" | cut -f2 -d\ )
+
+makelist ()
+{
+    DIRNAME=`dirname "$1"`
+    LISTFILE=`basename "$1"`
+    PRODUCT=${LISTFILE%%.list}
+
+    cd $DIRNAME
+
+    VERSION=$(grep %version "$LISTFILE" | head -1 \
+                            | sed -e "s/$(echo -e '\t')/ /g" | cut -f2 -d\ )
+    RELEASE=$(grep %release "$LISTFILE" | head -1 \
+                            | sed -e "s/$(echo -e '\t')/ /g" | cut -f2 -d\ )
 
     if [ "$RELEASE" != "" ] && [ "$RELEASE" -ne 0 ]; then
         VERSION="$VERSION-$RELEASE"
     fi
 
-    cd "$DIRNAME"
-    DEBFILE="`find . -type f -name \"$PRODUCT-$VERSION*.deb\" -newer \"$LISTFILE\"`"
-    if [[ "$LISTFILE" -nt "$DEBFILE" ]]; then
-        echo Rebuilding $PRODUCT...
-        find -name Makefile -execdir make -f '{}' \;
-        # Decide on whether to package for specific architecture or for all
-        # Look for binary files
-        find -type f -execdir file '{}' \; | grep -E ':[ \t]*(ELF|Mach-O).*executable.*(386|x86-64)' > /dev/null
-        if [ $? -eq 0 ] ; then
-            # Found Package for architecture
-            sudo epm -f deb "$PRODUCT"
-        else
-            # Package for noarch (in Debian: "all")
-            sudo epm -f deb -a all "$PRODUCT"
-        fi
+    DEBFILE=`find . -type f -name "$PRODUCT-$VERSION"*.deb`
+    if [ "$LISTFILE" -nt "$DEBFILE" ]; then
+        echo "Rebuilding [00;33m$PRODUCT[00m..."
+        find . -name Makefile -execdir make -f '{}' \;
+        sudo epm -f deb "$PRODUCT"
     fi
+
+    cd $OLDPWD
 }
 
-for LIST in "$@"; do
-    if [ -d "$LIST" ]; then
-        for LIST2 in "$LIST"/*.list; do
-            makelist "$LIST2"
-        done
-    else
-        makelist "$LIST"
+
+#
+# main ()
+{
+    if [ $# -eq 0 ]; then
+        usage
+        exit 1
     fi
-done
+
+    for LIST in "$@"; do
+        if [ -d "$LIST" ]; then
+            #
+            # This will build entire sub-tree under $LIST. Of course if that is
+            # required, ie. If `.list' file is newer than `.deb' file.
+            #
+            for LIST2 in `find $LIST -name \*.list`; do
+                makelist "$LIST2"
+            done
+        else
+            makelist "$LIST"
+        fi
+    done
+}
